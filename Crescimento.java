6ff64efe5ae67a80a32becfb7c205488a9ff23ab
Giovanni <giovanniaugusto20@gmail.com>
2025-08-25 10:52:21 -0300

    Exercicio 18

---
+++ Crescimento.java
@@ -0,0 +1,23 @@
+public class Crescimento {
+    public static void main(String[] args) {
+
+        //Alturas iniciais em cm
+        int alturaFrancisco = 150;
+        int alturaSara = 110;
+
+        //crescimento em cm
+        int cresceimentoFrancisco = 2;
+        int crescimentoSara = 3;
+
+        int anos = 0;
+
+        while (alturaSara <= alturaFrancisco){
+            alturaFrancisco += cresceimentoFrancisco;
+            alturaSara += crescimentoSara;
+            anos ++;
+        }
+        System.out.println("Sara sera maior que Francisco em " + anos + " anos");
+        System.out.println("Altura final de sara: " + alturaSara + "cm");
+        System.out.println("Altura final de Francisco: " + alturaFrancisco + "cm");
+    }
+}
